cbd9a0ee2b994ad557e53fa4f06c69ca792e9f73
Jonah Nye <Jonahnye94@gmail>
2019-05-25 13:17:39 -0400

    data base config early stage

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,11 @@
 "use strict";
 const express = require("express");
 const routes = express.Router();
-const pool = require("../connection/connection.js");
+const pool = require("../connection/pg-connection-pool.js");
+
+pool.query("SELECT * FROM log").then((result) => {
+    console.log(result.rows);
+})
+
+
+module.exports = routes;
